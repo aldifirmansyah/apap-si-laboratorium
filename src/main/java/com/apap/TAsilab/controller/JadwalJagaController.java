d5afe901f404b2eea9cb23720bd2b218f1b972e9
ghalsa <ghina.almira.salsabila@gmail.com>
2018-12-09 17:50:46 +0700

    insyaalloh selesai

--- src/main/java/com/apap/TAsilab/controller/JadwalJagaController.java
+++ src/main/java/com/apap/TAsilab/controller/JadwalJagaController.java
@@ -1,22 +1,21 @@
 package com.apap.TAsilab.controller;
 
 import java.io.IOException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -61,58 +60,171 @@ public class JadwalJagaController {
 	}
 	
 	private Map<Integer, String> getStaff() throws Exception, IOException, Throwable {
-		System.out.println("masuk map");
+//		System.out.println("masuk map");
 		List <StaffDetail> staffList  = this.getAllStaff();
 		Map<Integer, String> infoStaff = new HashMap<Integer, String>();
 		for(StaffDetail staff : staffList) {
 			infoStaff.put(staff.getId(), staff.getNama());
 		}
-		System.out.println(infoStaff.size());
-		System.out.println(infoStaff.get(633));
+//		System.out.println(infoStaff.size());
+//		System.out.println(infoStaff.get(633));
 		return infoStaff;
+	}
+	
+	public class JadwalJagaListModel{
+		private ArrayList<JadwalJagaModel> listJadwalJaga;
+		public ArrayList<JadwalJagaModel> getListJadwalJaga(){
+			return listJadwalJaga;
+		}
+		public void setListJadwalJaga(ArrayList<JadwalJagaModel> listJadwalJaga) {
+			this.listJadwalJaga = listJadwalJaga;
+		}
 	}
 	
 	//belum yg tambah banyak
 	@RequestMapping(value = "/lab/jadwal-jaga/tambah", method = RequestMethod.GET)
-	public String addJadwalJaga(Model model) throws Exception{		
-	
-		model.addAttribute("jadwalJaga", new JadwalJagaModel());
+	public String addJadwalJaga(Model model) throws Exception{
+		JadwalJagaListModel listJadwalJaga = new JadwalJagaListModel();		
+		ArrayList<JadwalJagaModel> jadwalJaga = new ArrayList<JadwalJagaModel>();
+		jadwalJaga.add(new JadwalJagaModel());
+		listJadwalJaga.setListJadwalJaga(jadwalJaga);
+		
 		List<StaffDetail> listStaff = this.getAllStaff();
+		model.addAttribute("jadwalJaga", listJadwalJaga);		
 		model.addAttribute("listStaff", listStaff);
-		return "addJadwalJaga";
-			
+		return "addJadwalJaga";				
 	}
 	
-//	@RequestMapping(value = "/lab/jadwal-jaga/tambah", method = RequestMethod.POST, params= {"addRow"})
-//	public String addRow(@ModelAttribute JadwalJagaModel jadwalJaga, Model model) throws Exception{	
-//		return "addJadwalJaga";
-//	}
-//	
-//	@RequestMapping(value="/lab/jadwal-jaga/tambah", method = RequestMethod.POST, params={"deleteRow"})
-//	public String deleteRow(@ModelAttribute JadwalJagaModel jadwalJaga, BindingResult bindingResult, HttpServletRequest req,Model model) {
-//		return "addJadwalJaga";
-//	}
-	
-	@RequestMapping(value = "/lab/jadwal-jaga/tambah", method = RequestMethod.POST)
-	public String addJadwalJagaSubmit(@ModelAttribute JadwalJagaModel jadwalJaga, Model model){
-
-		//ini untuk handle agar date yang dimasukkan bukan date yang sudah berlalu
-		if (jadwalJaga.getTanggal().before(new Date())) {
-			model.addAttribute("msg", "date yang dimasukkan sudah berlalu");
-			return "failed-date-passed";
+	@RequestMapping(value = "/lab/jadwal-jaga/tambah", method = RequestMethod.POST, params= {"add-more"})
+	public String addRow(@ModelAttribute JadwalJagaListModel JadwalJaga, Model model) throws Exception{	
+		
+		if(JadwalJaga.getListJadwalJaga()==null) {
+			ArrayList<JadwalJagaModel> jadwalJagaList = new ArrayList<JadwalJagaModel>();
+			jadwalJagaList.add(new JadwalJagaModel());
+			JadwalJaga.setListJadwalJaga(jadwalJagaList);
 		}
 		else {
-			try {
-				restTemplate.postForObject("http://localhost:6060/testing/kirim-jadwal", jadwalJaga, ResponseEntity.class);
-				//link diganti sama web service yg dibuat igd
+			
+			String strWaktuSelesai = JadwalJaga.getListJadwalJaga().get(JadwalJaga.getListJadwalJaga().size()-1).getWaktuSelesai();
+			String strWaktuMulai = JadwalJaga.getListJadwalJaga().get(JadwalJaga.getListJadwalJaga().size()-1).getWaktuMulai();
+			Date parseWaktuSelesai=new SimpleDateFormat("hh:mm").parse(strWaktuSelesai);
+			Date parseWaktuMulai=new SimpleDateFormat("hh:mm").parse(strWaktuMulai);
+			
+			java.sql.Time inputWaktuSelesai = new java.sql.Time(parseWaktuSelesai.getTime());
+			java.sql.Time inputWaktuMulai = new java.sql.Time(parseWaktuMulai.getTime());
+			
+			if(inputWaktuSelesai.before(inputWaktuMulai)) {
+				model.addAttribute("msg", "tolong masukkan jam selesai setelah jam mulai");
+				return "failed-date-passed";
 			}
-			catch(Exception e) {
-				
-			}
-			jadwalJagaService.addJadwalJaga(jadwalJaga);
-			model.addAttribute("msg", "jadwal berhasil ditambah");
-			return "success-page";
+			else {
+				JadwalJaga.getListJadwalJaga().add(new JadwalJagaModel());
+			}			
 		}
+		
+		List<StaffDetail> listStaff = this.getAllStaff();		
+		model.addAttribute("jadwalJaga", JadwalJaga);		
+		model.addAttribute("listStaff", listStaff);
+		return "addJadwalJaga";	
+	}
+	
+	@RequestMapping(value = "/lab/jadwal-jaga/tambah", method = RequestMethod.POST)
+	public String addJadwalJagaSubmit(@ModelAttribute JadwalJagaListModel JadwalJaga, Model model) throws ParseException{
+		
+		String msg = "";
+		LocalDate current = LocalDate.now();
+		LocalTime currentTime = LocalTime.now();
+		
+		for(int i=0; i<JadwalJaga.getListJadwalJaga().size(); i++) {
+			int idxJadwal = i+1;
+
+			//cek date
+			java.sql.Date inputDate = JadwalJaga.getListJadwalJaga().get(i).getTanggal();
+			LocalDate inputLocalDate = inputDate.toLocalDate();
+			
+			boolean isEqual = inputLocalDate.isEqual(current);
+			boolean isBefore = inputLocalDate.isBefore(current);
+			boolean isAfter = inputLocalDate.isAfter(current);
+			
+			//cek time
+			String strWaktuSelesai = JadwalJaga.getListJadwalJaga().get(i).getWaktuSelesai();
+			String strWaktuMulai = JadwalJaga.getListJadwalJaga().get(i).getWaktuMulai();
+			Date parseWaktuSelesai=new SimpleDateFormat("hh:mm").parse(strWaktuSelesai);
+			Date parseWaktuMulai=new SimpleDateFormat("hh:mm").parse(strWaktuMulai);
+			
+			java.sql.Time inputWaktuSelesai = new java.sql.Time(parseWaktuSelesai.getTime());
+			java.sql.Time inputWaktuMulai = new java.sql.Time(parseWaktuMulai.getTime());
+			
+			if(isAfter) {
+//				System.out.println("jadwal jaga ke " + idxJadwal +  " masuk after");
+								
+				if(inputWaktuSelesai.before(inputWaktuMulai)) {
+					model.addAttribute("msg", "tolong masukkan jam selesai setelah jam mulai");
+					return "failed-date-passed";
+				}
+				else {
+					
+					try {
+						restTemplate.postForObject("http://localhost:6060/testing/kirim-jadwal", JadwalJaga.getListJadwalJaga().get(i), ResponseEntity.class);
+						//link diganti sama web service yg dibuat igd
+					}
+					catch(Exception e) {
+						
+					}
+					
+					jadwalJagaService.addJadwalJaga(JadwalJaga.getListJadwalJaga().get(i));
+					msg += "jadwal jaga ke " + idxJadwal +  " berhasil dimasukkan" + ". ";
+				}
+			}
+			else {	
+				if(isEqual) {
+//					System.out.println("jadwal jaga ke " + idxJadwal +  " masuk equal");
+					
+					String strInputTime = JadwalJaga.getListJadwalJaga().get(i).getWaktuMulai();
+					Date strParse=new SimpleDateFormat("hh:mm").parse(strInputTime);
+					
+					java.sql.Time inputTime = new java.sql.Time(strParse.getTime());
+					LocalTime inputLocalTime = inputTime.toLocalTime();
+					
+					boolean timeEqual = inputLocalTime.equals(currentTime);
+					boolean timeBefore = inputLocalTime.isBefore(currentTime);
+					boolean timeAfter = inputLocalTime.isAfter(currentTime);
+										
+					 if(!timeBefore) {
+						 						
+							if(inputWaktuSelesai.before(inputWaktuMulai)) {
+								model.addAttribute("msg", "tolong masukkan jam selesai setelah jam mulai");
+								return "failed-date-passed";
+							}
+							else {
+								
+								try {
+									restTemplate.postForObject("http://localhost:6060/testing/kirim-jadwal", JadwalJaga.getListJadwalJaga().get(i), ResponseEntity.class);
+									//link diganti sama web service yg dibuat igd
+								}
+								catch(Exception e) {
+									
+								}
+								
+								jadwalJagaService.addJadwalJaga(JadwalJaga.getListJadwalJaga().get(i));
+								msg += "jadwal jaga ke " + idxJadwal +  " berhasil dimasukkan" + ". ";
+							}
+							
+					 }
+					 else {
+						 msg += "jadwal jaga ke " + idxJadwal +  " memiliki jam mulai yang dimasukkan sudah berlalu" + ". ";
+					 }
+					
+				}
+				else {
+//					System.out.println("jadwal jaga ke " + idxJadwal +  " masuk before");
+					msg += "jadwal jaga ke " + idxJadwal +  " memiliki date yang dimasukkan sudah berlalu" + ". ";
+				}
+			}
+						
+		}		
+		model.addAttribute("msg", msg);
+		return "success-page";
 	}
 	
 	@RequestMapping(value = "/lab/jadwal-jaga/pilihTanggal", method = RequestMethod.GET)
@@ -145,7 +257,7 @@ public class JadwalJagaController {
 				staffJaga = listStaff.get(i).getNama(); 
 			}
 		}
-		System.out.println(staffJaga);
+//		System.out.println(staffJaga);
 		model.addAttribute("staffJaga", staffJaga);
 		model.addAttribute("listStaff", listStaff);
 		return "ubah-jadwal-jaga";
@@ -155,22 +267,120 @@ public class JadwalJagaController {
 	
 	//cek lagi seharusnya outputnya gimana
 	@RequestMapping(value = "/lab/jadwal-jaga/ubah/{id}", method = RequestMethod.POST)
-	public String ubahJadwalJagaSubmit(@PathVariable(value="id") int id, Model model, @ModelAttribute JadwalJagaModel newJadwalJaga){
+	public String ubahJadwalJagaSubmit(@PathVariable(value="id") int id, Model model, @ModelAttribute JadwalJagaModel newJadwalJaga) throws ParseException{
 		
-		//ini untuk handle agar date yang dimasukkan bukan date yang sudah berlalu
-		//kenapa yg di cek jadwal yg lama bukan jadwal yg barunya?
-		if (newJadwalJaga.getTanggal().before(new Date())) {
-			model.addAttribute("msg", "date yang dimasukkan sudah berlalu");
-			return "failed-date-passed";
+//		//ini untuk handle agar date yang dimasukkan bukan date yang sudah berlalu
+//		//kenapa yg di cek jadwal yg lama bukan jadwal yg barunya?
+//		if (newJadwalJaga.getTanggal().before(new Date())) {
+//			model.addAttribute("msg", "date yang dimasukkan sudah berlalu");
+//			return "failed-date-passed";
+//		}
+//		else {
+//			jadwalJagaService.ubahJadwalJaga(newJadwalJaga.getId(), newJadwalJaga);
+//			model.addAttribute("msg", "jadwal berhasil diubah");
+//			return "success-page";
+//		}	
+
+		String msg = "";
+		
+		//cek date
+		LocalDate current = LocalDate.now();
+		LocalTime currentTime = LocalTime.now();
+
+		java.sql.Date inputDate = newJadwalJaga.getTanggal();
+		LocalDate inputLocalDate = inputDate.toLocalDate();
+		
+		boolean isEqual = inputLocalDate.isEqual(current);
+		boolean isBefore = inputLocalDate.isBefore(current);
+		boolean isAfter = inputLocalDate.isAfter(current);
+		
+		
+		//cek time
+		String strWaktuSelesai = newJadwalJaga.getWaktuSelesai();
+		String strWaktuMulai = newJadwalJaga.getWaktuMulai();
+		Date parseWaktuSelesai=new SimpleDateFormat("hh:mm").parse(strWaktuSelesai);
+		Date parseWaktuMulai=new SimpleDateFormat("hh:mm").parse(strWaktuMulai);
+		
+		java.sql.Time inputWaktuSelesai = new java.sql.Time(parseWaktuSelesai.getTime());
+		java.sql.Time inputWaktuMulai = new java.sql.Time(parseWaktuMulai.getTime());
+		
+		if(isAfter) {
+			
+			if(inputWaktuSelesai.before(inputWaktuMulai)) {
+				model.addAttribute("msg", "tolong masukkan jam selesai setelah jam mulai");
+				return "failed-date-passed";
+			}
+			else {
+				
+				try {
+					restTemplate.postForObject("http://localhost:6060/testing/kirim-jadwal", newJadwalJaga, ResponseEntity.class);
+					//link diganti sama web service yg dibuat igd
+				}
+				catch(Exception e) {
+					
+				}
+				
+				jadwalJagaService.addJadwalJaga(newJadwalJaga);
+				msg = "jadwal jaga berhasil di update";
+			}			
+			
 		}
-		else {
-			jadwalJagaService.ubahJadwalJaga(newJadwalJaga.getId(), newJadwalJaga);
-			model.addAttribute("msg", "jadwal berhasil diubah");
-			return "success-page";
-		}	
-		
+		else {	
+			if(isEqual) {
+//				try {
+//					restTemplate.postForObject("http://localhost:6060/testing/kirim-jadwal", newJadwalJaga, ResponseEntity.class);
+//					//link diganti sama web service yg dibuat igd
+//				}
+//				catch(Exception e) {
+//					
+//				}
+//				jadwalJagaService.addJadwalJaga(newJadwalJaga);
+//				msg = "jadwal jaga berhasil di update";
+								
+				String strInputTime = newJadwalJaga.getWaktuMulai();
+				Date strParse=new SimpleDateFormat("hh:mm").parse(strInputTime);
+				
+				java.sql.Time inputTime = new java.sql.Time(strParse.getTime());
+				LocalTime inputLocalTime = inputTime.toLocalTime();
+				
+				boolean timeEqual = inputLocalTime.equals(currentTime);
+				boolean timeBefore = inputLocalTime.isBefore(currentTime);
+				boolean timeAfter = inputLocalTime.isAfter(currentTime);
+									
+				 if(!timeBefore) {
+					 if(inputWaktuSelesai.before(inputWaktuMulai)) {
+							model.addAttribute("msg", "tolong masukkan jam selesai setelah jam mulai");
+							return "failed-date-passed";
+						}
+						else {
+							
+							try {
+								restTemplate.postForObject("http://localhost:6060/testing/kirim-jadwal", newJadwalJaga, ResponseEntity.class);
+								//link diganti sama web service yg dibuat igd
+							}
+							catch(Exception e) {
+								
+							}
+							
+							jadwalJagaService.addJadwalJaga(newJadwalJaga);
+							msg = "jadwal jaga berhasil di update";
+						}
+				 }
+				 else {
+					 msg = "jadwal jaga memiliki jam mulai yang dimasukkan sudah berlalu" + ". ";
+				 }
+			}
+			else {
+				msg = "jadwal jaga tidak berhasil di update, date sudah berlalu";
+			}
+		}
+							
+		model.addAttribute("msg", msg);
+		return "success-page";
 		
 	}	
+	
+	
 	
 	
 }
